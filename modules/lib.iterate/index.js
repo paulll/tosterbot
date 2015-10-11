fadea99ffaa511955d85997b35bab567b2775733
paulll <paulll@derpy.ru>
2015-10-11 13:29:40 +0300

    Модуль асинхронной итерации по итераторам

---
+++ modules/lib.iterate/index.js
@@ -0,0 +1,16 @@
+api.lib.iterate = {};
+
+api.lib.iterate.reduce = function reduce (iter, ac, fn, cb) {
+	let item = iter.next();
+	if (item.done) {
+		return cb(null, ac);
+	}
+	fn (ac, item.value, function (error, data) {
+		if (error) {
+			return cb(error);
+		}
+		setTimeout(function() {
+			reduce(iter, data, fn, cb);
+		}, 0);
+	});
+};
