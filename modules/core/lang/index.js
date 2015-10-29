048d9d1db1ce2e0c40fce485e0a52b0ee9ac43fe
paulll <paulll@derpy.ru>
2015-10-29 17:02:02 +0300

    Слой абстракции

---
+++ modules/core/lang/index.js
@@ -0,0 +1,31 @@
+var reduce = api.lib.iterate.reduce,
+	handleError = api.lib.debug.handleError,
+	level = api.lib.debug.level;
+
+api.lang.generateSimple = function (message, callback) {
+	reduce(api.lang.generators.entries(), {confidence: 0}, function (current, generator, callback) {
+		generator.generate(message, function (error, probablyGeneratedMessage) {
+			if (handleError(error, level.warn, callback)) {
+				if (current.confidence > probablyGeneratedMessage.confidence) {
+					callback(error, current);
+				} else {
+					callback(error, probablyGeneratedMessage);
+				}
+			}
+		});
+	}, callback);
+}
+
+api.lang.parseSimple = function (message, callback) {
+	reduce(api.lang.parsers.entries(), {confidence: 0}, function (current, parser, callback) {
+		parser.parse(message, function (error, probablyParsedMessage) {
+			if (handleError(error, level.warn, callback)) {
+				if (current.confidence > probablyParsedMessage.confidence) {
+					callback(error, current);
+				} else {
+					callback(error, probablyParsedMessage);
+				}
+			}
+		});
+	}, callback);
+}
