74afdc8da2df05cca0cfcade089110e8fcf48381
paulll <paulll@derpy.ru>
2015-10-29 17:04:15 +0300

    Убрал следы отладки

--- modules/core/memory/index.js
+++ modules/core/memory/index.js
@@ -2,8 +2,6 @@ var reduce = api.lib.iterate.reduce,
 	handleError = api.lib.debug.handleError,
 	level = api.lib.debug.level;
 
-var $ = console.log.bind(console);
-
 api.memory.getSimple = function (subject, callback) {
 	reduce(api.memory.providers.values(), null, function (current, provider, rcallback) {
 		provider.get(subject, null, null, function (error, result) {
