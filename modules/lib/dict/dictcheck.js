21283cdbd95fe1b570524d561c54ded398de7cb2
paulll <paulll@derpy.ru>
2015-11-05 17:27:05 +0300

    Переписал систему сравнения Добавлено: - Символ-джокер * - Группы #var = ... - Имена &(name, %var%)

--- modules/lib/dict/dictcheck.js
+++ modules/lib/dict/dictcheck.js
@@ -1,89 +1,130 @@
 "use strict";
 
-/**
- * Если множество
- *   Для каждого значения
- *     Если значение - строка
- *       Если она подходит
- *         Вернуть остаток строки
- *     Если не строка
- *       Возвращать результаты рекурсии
- * Если массив
- *   Если первый элемент - строка
- *     Если она подходит
- *       Если остались еще элементы массива
- *         Возвращаем рекурсию от оставшихся элементов
- *       Если массив остался пустым
- *         Вернуть остаток строки
- *   Если не строка
- *     Для каждого варианта остатка строки
- *       Если остались еще элементы
- *         Возвращаем рекурсию от оставшихся элементов
- *       Если массив остался пустым
- *         Вернуть остаток строки
- */
+class Scope extends Map {
+	constructor (root, name) {
 
-function TokenList (rest, part) {
-	this.rest = rest.rest.substr(part.length);
-	this.stack = rest.stack.concat([part]);
-	this.substr = String.prototype.substr.bind(this.rest);
-	this.indexOf = String.prototype.indexOf.bind(this.rest);
-}
+		super(root);
 
-function Input (start) {
-	this.rest = start;
-	this.stack = [];
-	this.substr = String.prototype.substr.bind(this.rest);
-	this.indexOf = String.prototype.indexOf.bind(this.rest);
-}
-
-function* check (rest, tree) {
-	if (tree instanceof Set) {
-		for (let value of tree) {
-			if (typeof value === 'string') {
-				if (rest.indexOf(value) === 0) {
-					yield new TokenList(rest, value);
+		if (root && root.name) {
+			if (name) {
+				this.name = `${root.name}.${name}`;
+				this.roots = root.roots.concat([this.name]);
+			} else {
+				this.name = root.name; 
+				this.roots = root.roots;
+			}
+		} else {
+			if (name) {
+				this.name = name;
+				if (root) {
+					this.roots = root.roots.concat([this.name]);
+				} else {
+					this.roots = [name];
 				}
 			} else {
-				yield* check(rest, value);
+				this.name = '';
+				this.roots = (root&&root.roots) || [];
+			}
+		}
+	}
+
+	inherit (name) {
+		if (name) return new Scope(this, name)
+		return this;
+	}
+
+	append (namespace) {
+		if (namespace) {
+			for (let i of namespace.entries()) {
+				if (!this.get(i[0])) {
+					this.set(i[0], i[1]);
+				}
+			}
+		}
+		return this;
+	}
+
+	write (string) {
+		for (let name of this.roots) {
+			let prop = this.get(name) || new Set;
+			prop.value = ((prop&&prop.value) || '') + string;
+			this.set(name, prop);
+		}
+		return this;
+	}
+
+	setGroup (groupName) {
+		if (groupName) {
+			for (let name of this.roots) {
+				let prop = this.get(name) || new Set;
+				prop.add(groupName);
+				this.set(name, prop);
+			}
+		}
+		return this;
+	}
+}
+
+function* recursionString(state, string, namespace) {
+	if (string == '*') {
+		for (let i=0,l=state.rest.length+1; i<l; i++) {
+			yield {
+				rest: state.rest.substr(i),
+				scope: namespace.append(state.scope).write(state.rest.substr(0, i))
 			}
 		}
 	} else {
-		if (typeof tree[0] === 'string') {
-			if (rest.indexOf(tree[0]) === 0) {
-				if (tree.length - 1) {
-					yield* check(new TokenList(rest, tree[0]), tree.slice(1));
-				} else {
-					yield new TokenList(rest, tree[0]);
-				}
-			}
-		} else {
-			for (let str of check(rest, tree[0])) {
-				if (tree.length - 1) {
-					yield* check(str, tree.slice(1));
-				} else {
-					yield str;
-				}
+		if (state.rest.indexOf(string) === 0) {
+			yield {
+				rest: state.rest.substr(string.length),
+				scope: namespace.append(state.scope).write(string)
 			}
 		}
 	}
 }
 
-function* check (rest, tree) {
+function* recursionArray(state, array, namespace) {
+	if (array.length) {
+		let firstItem = array[0];
 
-}
-
-function restore(k, v) {
-	if (v.hasOwnProperty('v')) {
-		return new Set(v.v);
+		for (let version of recursionUnknown(state, firstItem, namespace)) {
+			if (array.length == 1) {
+				yield version;
+			} else {
+				yield* recursionUnknown(version, array.slice(1), namespace);
+			}
+		}
 	}
-	return v;
 }
 
+function* recursionSet(state, set, namespace) {
+
+	for (let subset of set) {
+		for (let d of recursionUnknown(state, subset, namespace.inherit(set.name).setGroup(set.group))) {
+			yield d;
+		}
+	}
+}
+
+function* recursionUnknown(state, o, namespace) {
+	if (Array.isArray(o)) {
+		yield* recursionArray(state, o, namespace)
+	} else if (o instanceof Set) {
+		yield* recursionSet(state, o, namespace)
+	} else if (typeof o === 'string') {
+		yield* recursionString(state, o, namespace);
+	} else {
+		throw new TypeError('What teh fuck?');
+	}
+}
+
+
+// main
 
 module.exports = function (ast, string) {
+
 	var success = false;
-	let generator = check(new Input(string), ast);
+	let generator = recursionUnknown({rest: string}, ast, new Scope);
 
 	for (var value of generator) {
 		if (value.rest == '') {
@@ -92,5 +133,16 @@ module.exports = function (ast, string) {
 		}
 	}
 
-	return success ? value.stack : success;
+	return success ? value : success;
+}
+
+// util
+
+
+
+function ns (last, add) {
+	if (!add) return last;
+	return last 
+		? last + '.' + add 
+		: add;
 }
