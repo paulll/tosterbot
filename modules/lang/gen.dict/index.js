160518ed637d1770087b5beb59c5c088cab99d74
paulll <paulll@derpy.ru>
2015-11-05 17:30:52 +0300

    Переход на новую систему модулей

--- modules/lang/gen.dict/index.js
+++ modules/lang/gen.dict/index.js
@@ -1,18 +1,29 @@
-api.lang.generators.dict = {};
-api.lang.generators.dict.generate = function (message, callback) {
-	
-	var answer = api.lib.dict.generate(message.answer);
+"use strict";
 
-	if (!answer) {
-		callback(null, {confidence: 0});
-	} else {
-		answer.confidence = answer.confidence || 1;
-		callback(null, answer);
-	}
-};
+var MessageGenerator = api.lib.support.MessageGenerator,
+	generate = api.lib.dict.generate;
 
-api.lib.dict.loadDict('./dict/ru/output/base.dict', function(error) {
-	if (error) {
-		console.log('[FAIL]', 'Should load base output dict');
+class DictGenerator extends MessageGenerator {
+
+	constructor() {
+		super();
+		api.lib.dict.loadDict('./dict/ru/output/base.dict', function(error) {
+			if (error) {
+				console.log('[FAIL]', 'Should load base output dict');
+			}
+		});
 	}
-});
+
+	generate (message, callback) {
+		var answer = generate(message.answer, message.answerVariables);
+
+		if (!answer) {
+			callback(null, {confidence: 0});
+		} else {
+			answer.confidence = answer.confidence || 1;
+			callback(null, answer);
+		}
+	}
+}
+
+api.lang.generators.add(new DictGenerator);
